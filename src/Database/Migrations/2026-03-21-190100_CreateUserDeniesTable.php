7405e500f2e70a7dc1c187a618fbf00453a68e3c
Ian Otieno Ochieng <ochiengian383@gmail.com>
2026-03-22 14:48:56 +0300

    deps: update vima/core dependency to ^0.2.7

--- src/Database/Migrations/2026-03-21-190100_CreateUserDeniesTable.php
+++ src/Database/Migrations/2026-03-21-190100_CreateUserDeniesTable.php
@@ -3,51 +3,82 @@
 namespace Vima\CodeIgniter\Database\Migrations;
 
 use CodeIgniter\Database\Migration;
+use Vima\Core\Support\FrameworkIntegration;
 
 class CreateUserDeniesTable extends Migration
 {
     public function up()
     {
-        $this->forge->addField([
-            'id' => [
-                'type'           => 'INT',
-                'constraint'     => 11,
-                'unsigned'       => true,
-                'auto_increment' => true,
-            ],
-            'user_id' => [
-                'type'           => 'INT',
-                'constraint'     => 11,
-                'unsigned'       => true,
-            ],
-            'permission_id' => [
-                'type'           => 'INT',
-                'constraint'     => 11,
-                'unsigned'       => true,
-            ],
-            'namespace' => [
-                'type'          => 'VARCHAR',
-                'constraint'    => 255,
-                'null'          => true,
-            ],
-            'reason' => [
-                'type'          => 'VARCHAR',
-                'constraint'    => 255,
-                'null'          => true,
-            ],
-            'created_at' => [
-                'type' => 'DATETIME',
-                'null' => true,
-            ],
-        ]);
+        $schema = FrameworkIntegration::getSchema();
+        $tables = FrameworkIntegration::requiredTables();
+        $table  = $schema->getTable($tables->userDenies);
 
-        $this->forge->addKey('id', true);
-        $this->forge->addUniqueKey(['user_id', 'permission_id', 'namespace']);
-        $this->forge->createTable('user_denies', true);
+        if (!$table) {
+            return;
+        }
+
+        $forgeFields = [];
+
+        foreach ($table->fields as $field) {
+            $ciField = [];
+            $ciType = 'VARCHAR';
+
+            switch ($field->type) {
+                case 'integer':
+                    $ciType = 'INT';
+                    if (!isset($field->length)) {
+                        $ciField['constraint'] = 11;
+                    }
+                    break;
+                case 'string':
+                    $ciType = 'VARCHAR';
+                    $ciField['constraint'] = $field->length ?? 100;
+                    break;
+                case 'text':
+                case 'json':
+                    $ciType = 'TEXT';
+                    break;
+                case 'datetime':
+                    $ciType = 'DATETIME';
+                    break;
+            }
+
+            $ciField['type'] = $ciType;
+
+            if ($field->nullable) {
+                $ciField['null'] = true;
+            }
+
+            if ($field->unsigned) {
+                $ciField['unsigned'] = true;
+            }
+
+            if ($field->autoIncrement) {
+                $ciField['auto_increment'] = true;
+            }
+
+            $forgeFields[$field->name] = $ciField;
+        }
+
+        $this->forge->addField($forgeFields);
+
+        foreach ($table->primaryKeys as $pk) {
+            $this->forge->addKey($pk, true);
+        }
+
+        foreach ($table->uniqueKeys as $uk) {
+            $this->forge->addUniqueKey($uk);
+        }
+
+        foreach ($table->foreignKeys as $fk) {
+            $this->forge->addForeignKey($fk->column, $fk->onTable, $fk->onColumn, $fk->onUpdate, $fk->onDelete);
+        }
+
+        $this->forge->createTable($tables->userDenies, true);
     }
 
     public function down()
     {
-        $this->forge->dropTable('user_denies', true);
+        $this->forge->dropTable(FrameworkIntegration::requiredTables()->userDenies, true);
     }
 }
